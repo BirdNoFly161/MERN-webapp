ecb219b58d82242ae03185b6c18574e0e758afc0
BirdNoFly161 <sharp.oussama@gmail.com>
2023-07-30 08:06:21 +0100

    Co-authored-by: Chadjaa Sofianne <chadjaa-sofiane@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -3,24 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-<<<<<<< Updated upstream
-
-/*const {MongoClient} = require("mongodb");
-const uri = "mongodb://127.0.0.1:27017";
-const client = new MongoClient(uri);
-
-async function run(){
-    const database = client.db('web-app-db');
-    console.log("connected to database successfully")
-    const users_collection = database.collection('users');
-    const users = await users_collection.find({}).toArray()
-    console.log(users)
-}
-
-run().catch(console.dir)
-*/
-=======
->>>>>>> Stashed changes
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,23 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-(async function (){
-  const database = await require('../database/connection');
-  const user_collection = database.collection('users');
-  let users = await user_collection.find({}).toArray()
-  console.log(users)
-})()
-//const user_collections = database.collection('users');
-=======
 var register_user = require('../controllers/register_user');
 
->>>>>>> Stashed changes
-=======
-var register_user = require('../controllers/register_user');
-
->>>>>>> Stashed changes
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
